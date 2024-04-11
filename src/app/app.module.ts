41980adb290524d4a7e8a7f55ceac04655a2fd73
shreyanshgupta123 <145015168+shreyanshgupta123@users.noreply.github.com>
2024-04-11 19:50:13 +0530

    11-04-2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApisComponent } from './Components/apis/apis.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApisComponent
   ],
   imports: [
     BrowserModule,
