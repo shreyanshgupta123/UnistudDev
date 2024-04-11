50d6860616f27fc47aacf2464b59c3699cc837d4
shreyanshgupta123 <145015168+shreyanshgupta123@users.noreply.github.com>
2024-04-12 00:36:32 +0530

    12-04-2024

--- src/app/Components/firstpage/firstpage.component.ts
+++ src/app/Components/firstpage/firstpage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-firstpage',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './firstpage.component.scss'
 })
 export class FirstpageComponent {
+  show:boolean=true;
+  showpage:boolean=false
+  constructor(private route:Router) { }
 
+  ngOnInit() {
+  }
+doc()
+{
+  this.route.navigate(['/doc']);
+
+}
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstpageComponent } from './Components/firstpage/firstpage.component';
+import { ApisComponent } from './Components/apis/apis.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:FirstpageComponent
+  },
+  {
+    path:'doc',component:ApisComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
